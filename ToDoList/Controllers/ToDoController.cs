b1fac0cd47534e3655a4c84d0e82422a6685a306
Garrett <garrettepperson@hotmail.com>
2019-08-16 13:37:47 -0500

    Removed unused dependencies

--- ToDoList/Controllers/ToDoController.cs
+++ ToDoList/Controllers/ToDoController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using ToDoList.ViewModels;
-using System.Web;
 using ToDoList.Enums;
 
 namespace ToDoList.Controllers
